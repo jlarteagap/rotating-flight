a3051a0279b880c64ff0652c1479a4d52f53e99b
Jorge <jorgelarteagap@gmail.com>
2022-07-21 17:00:26 -0400

    Finish proyect

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ let flights = [
   },
 ];
 
+const destinations = ["London", "Paris", "New York", "Tokyo", "Sydney", "Melbourne"]
+const remarks = ["On time", "Delayed", "Cancelled", "Unknown"]
+
+let hour = 15 
+
+
 function populateTable(){
     for(const flight of flights){
         const tableRow = document.createElement('tr')
@@ -18,13 +24,16 @@ function populateTable(){
             const tableCell = document.createElement('td')
 
             const word = Array.from (flight[flightDetail])
-            for(const letter of word){
+            
+            for(const [index, letter] of word.entries()){
                 const letterElement = document.createElement('div')
-                letterElement.classList.add('flip')
-                letterElement.textContent = letter
-                tableCell.append(letterElement)
+
+                setTimeout(() => {
+                    letterElement.classList.add('flip')
+                    letterElement.textContent = letter
+                    tableCell.append(letterElement)
+                }, 100* index)
             }
-            // tableCell.innerHTML = flight[flightDetail]
 
             tableRow.append(tableCell)
         }
@@ -33,3 +42,51 @@ function populateTable(){
 }
 
 populateTable()
+
+function generateRandomLetter(){
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    return alphabet[Math.floor(Math.random() * alphabet.length)]
+}
+
+function generateRandomNumber(maxNumber){
+    const numbers = "0123456789"
+    if(maxNumber){
+        const newNumbers = numbers.slice(0, maxNumber)
+        return newNumbers[Math.floor(Math.random() * newNumbers.length)]
+
+    }
+    return numbers.charAt(Math.floor(Math.random() * numbers.length))
+}
+
+function generateTime(){
+    let displayHour = hour
+
+    if(hour < 24){
+        hour++
+    }
+    if(hour >= 24){
+        hour = 1
+        displayHour = hour
+    }
+    if(hour < 10){
+        displayHour = "0" + hour
+    }
+
+    return displayHour + ":" + generateRandomNumber(5) + generateRandomNumber()
+}
+function shuffleUp() {
+    flights.shift()
+    flights.push({
+        time: generateTime(),
+        destination: destinations[Math.floor(Math.random() * destinations.length)],
+        flight: generateRandomLetter() + generateRandomLetter() + " " + generateRandomNumber() + generateRandomNumber() + generateRandomNumber(),
+        gate: generateRandomLetter() + " " + generateRandomNumber() + generateRandomNumber(),
+        remarks: remarks[Math.floor(Math.random() * remarks.length)],
+    })
+
+    tableBody.textContent = ""
+    populateTable()
+
+}
+
+setInterval(shuffleUp, 2000)
